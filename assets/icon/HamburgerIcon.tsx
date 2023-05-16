da630e76248751b9899bb5548e5c5f7e1f54ccee
AsimVeledarevic-mop <asim.veledarevic@ministryofprogramming.com>
2023-05-16 14:53:25 +0200

    Added assets

---
+++ assets/icon/HamburgerIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HamburgerIcon() {
+  return (
+    <svg
+      width="36"
+      height="28"
+      viewBox="0 0 36 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="36" height="6" rx="2" fill="#004743" />
+      <rect y="11" width="36" height="6" rx="2" fill="#004743" />
+      <rect y="22" width="36" height="6" rx="2" fill="#004743" />
+    </svg>
+  );
+}
+
+export default HamburgerIcon;
